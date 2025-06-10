718a30c18229abdc09fa5aed45e778843f58b80f
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-06-10 12:23:33 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/** Variáveis */
+btnSubmit = document.getElementById("btnSubmit");
+
+inputNome = document.getElementById("inputNome");
+
+inputDesc = document.getElementById("inputDesc");
+
+inputValor = document.getElementById("inputValor    ");
